14b1f3d527293c2488aa40c2bb6c50de16f8b416
Ville Rantala <ville.rantala@microsoft.com>
2018-01-11 16:16:57 +0200

    Added visibilities

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,15 +2,15 @@ pragma solidity ^0.4.4;
 
 contract Mortal {
     /* Define variable owner of the type address */
-    address owner;
+    address public owner;
 
     /* This function is executed at initialization and sets the owner of the contract */
-    function Mortal() {
+    function Mortal() public {
         owner = msg.sender;
     }
 
     /* Function to recover the funds on the contract */
-    function kill() {
+    function kill() public {
         if (msg.sender == owner) {
             selfdestruct(owner);
         }
@@ -19,10 +19,10 @@ contract Mortal {
 
 contract Greeter is Mortal {
     /* Define variable greeting of the type string */
-    string greeting = "Hello from the Truffle Greeter!";
+    string public greeting = "Hello from the Truffle Greeter!";
 
     /* The main function */
-    function greet() constant returns (string) {
+    function greet() public constant returns (string) {
         return greeting;
     }
 }
